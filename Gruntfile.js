8aec710614aa921a3703dfe54a5fbc0cce574726
kikoshoung <kiko.xiang@guohead.com>
2014-01-19 20:00:56 +0800

    file banner modified

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
-                banner: '/* <%= pkg.name %> - Canvas charts with radar impression\n * @author kikoshoung (kikoshoung@gmail.com)\n * last modified at <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %>\n */\n',
+                banner: '/* <%= pkg.name %> - Scratchcard - A scratch card library based on canvas\n * @author kikoshoung (kikoshoung@gmail.com)\n * last modified at <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %>\n */\n',
                 beautify: {
                     ascii_only: true
                 }
